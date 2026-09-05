39318edce3c63bdc52ab4a20bcb3518ee965827e
kasaburi <natiakasaburi2001w@gmail.com>
2026-09-05 16:00:46 +0400

    Update project

--- app/main.py
+++ app/main.py
@@ -42,11 +42,22 @@ app = FastAPI(
 )
 
 
+# =========================================================
+# CORS
+# =========================================================
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+
+    allow_origins=[
+        "http://127.0.0.1:5501",
+        "http://localhost:5501",
+    ],
+
     allow_credentials=True,
+
     allow_methods=["*"],
+
     allow_headers=["*"],
 )
 
